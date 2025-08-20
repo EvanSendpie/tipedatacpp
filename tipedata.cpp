afd72a5966ee8dbd40033e51e1d756065386f1bf
EvanSendpie <157936933+EvanSendpie@users.noreply.github.com>
2025-08-20 17:10:09 +0700

    add files

---
+++ tipedata.cpp
@@ -0,0 +1,41 @@
+#include <iostream> //include library i/o
+using namespace std; //menggunakan namespace std supaya tidak perlu menulis std::
+
+int main() {
+    //Tipe data integer - bilangan bulat
+    int angka = 25;
+
+    //Tipe data float - angka bertitik desimal
+    float angkaDesimal = 25.0;
+
+    //Tipe data karakter
+    char karakter = 'c';
+
+    //Tipe data boolean - bernilai true atau false
+    bool isBoolean = true;
+
+    //Tipe data string
+    string nama = "Fiony";
+    
+
+    //7 tipe data primitif c++
+    short pendek = 32767;
+    int integer = 2147483648;
+    long nim = 255150307111039;
+    float des = 3.141592653589;
+    double hutang = 9876543210;
+    char karakter = 'a';
+    bool boolean = false;
+
+    //Deklarasi variabel
+    //[Tipe Data] identifier = nilai awal;
+    //Contohnya:
+    int a;
+    float IPK, IPSem, IPBeban;
+    double min = 0, maks = 100;
+    bool cek, lulus = false;
+
+    //Konstanta - identifier untuk menyimpan nilai yang tidak bisa diubah valuenya
+    const int Nilai_A = 80;
+
+}
